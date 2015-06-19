79210a9b686e1149913db577aaf675c72fc15961
Arnis <arnislupiks@gmail.com>
2015-06-19 01:31:36 +0100

    This is first upload of project
    This is test project using Visual Studio.
    Created one class with test code to it

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        private string _lastName;
+        
+        public string LastName
+        {
+            get
+            {
+                //Any code here
+                return _lastName;
+            }
+            set
+            {
+                //Any code here
+                _lastName = value;
+            }
+        }
+        public string FirstName { get; set; }
+
+        public string EamilAddress { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+    }
+}
